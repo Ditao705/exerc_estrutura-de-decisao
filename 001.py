129fe16b464fbf353763a080394ae2f34dd8b5b0
Lorenzo sb <116968226+Ditao705@users.noreply.github.com>
2022-11-06 16:33:56 -0300

    Create 001.py

---
+++ 001.py
@@ -0,0 +1,10 @@
+""""1.Faça um Programa que peça dois números e imprima o maior deles.Faça um Programa que peça dois números e imprima o maior deles."""
+
+a1 = input("digite um numero: ")
+
+a2 = input("digite outro: ")
+
+if a1 > a2:
+    print(a1)
+else:
+    print(a2)
