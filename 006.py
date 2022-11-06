36ff7d0559370119e5408590b5ed01c81fc00488
Lorenzo sb <116968226+Ditao705@users.noreply.github.com>
2022-11-06 19:28:39 -0300

    Create 006.py

---
+++ 006.py
@@ -0,0 +1,14 @@
+"""Faça um Programa que leia três números e mostre o maior dele"""
+
+n1 = float(input("digite um numero: "))
+
+n2 = float(input("digite um numero: "))
+
+n3 = float(input("digite um numero: "))
+
+if n1 > n2 and n3:
+    print(n1)
+elif n2 > n1 and n3:
+    print(n2)
+elif n3 > n1 and n2:
+    print(n3)
