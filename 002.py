518ab9a700372ab9d81b1aa5c099dacbc9e459cb
Lorenzo sb <116968226+Ditao705@users.noreply.github.com>
2022-11-06 16:39:30 -0300

    Create 002.py

---
+++ 002.py
@@ -0,0 +1,9 @@
+"""2.Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo."""
+
+
+a1 = int(input("forneça um valor: "))
+
+if a1 < 0:
+    print("negativo")
+else:
+    print("positivo")
